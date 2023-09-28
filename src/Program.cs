61f587085caae3f44c966bb9da7c2a122c31b11a
lxgr-linux <lxgr@protonmail.com>
2023-09-28 14:00:09 +0200

    test

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System.Net;
 using CardchainCs.CardchainClient;
 using Cosmcs.Crypto.Secp256k1;
+using DecentralCardGame.Cardchain.Cardchain;
 using Google.Protobuf;
-using Grpc.Core;
-using Grpc.Net.Client;
-using Grpc.Net.Client.Web;
+
 
 System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 byte[] hex = new byte[32]
@@ -13,24 +12,20 @@ byte[] hex = new byte[32]
     176, 135, 218, 199, 231, 70, 249,
     129, 238, 212, 107, 207, 147, 217,
     51, 43, 217, 82, 136, 182, 245,
-    189, 104, 186, 17
+    189, 104, 186, 16
 };  // Place byte publikKey here
 
 var privateKey = new PrivateKey(hex);
 var accoutAddress = privateKey.PublicKey().AccountId("cc");
-var option = new GrpcChannelOptions
-{
-    HttpHandler = new GrpcWebHandler(new HttpClientHandler()),
-};
 Console.Out.WriteLine(accoutAddress);
-
-var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
 
 
 var ccClient = new CardchainClient("http://lxgr.xyz:9090", "Testnet3", hex);
-var resp = ccClient.SendMsgBuyCardScheme("10000000000000000000", "ucredits").Result;
-Console.Out.WriteLine(resp.RawResponse);
-if (resp.ResponseMessage != null)
+//var resp = ccClient.SendMsgBuyCardScheme("10000000000000000000", "ucredits").Result;
+//var resp = ccClient.SendMsgCreateCollection("jaja", accoutAddress.ToString(), accoutAddress.ToString(), new string[]{}).Result;
+var resp = ccClient.SendMsgExecMsgVoteCard("cc1axyyssx639esalxq85y6whckhc42vxk5fdxw5x", 431, "overpowered").Result;
+Console.Out.WriteLine(resp.ClientResponse.RawResponse);
+foreach (var respResponseMessage in resp.ResponseMessages)
 {
-    Console.Out.WriteLine(JsonFormatter.Default.Format(resp.ResponseMessage));
+    Console.Out.WriteLine(JsonFormatter.Default.Format(respResponseMessage));
 }
